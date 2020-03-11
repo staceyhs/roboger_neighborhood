1d407ca8ccc84046ba0231a779c9a22fdec7da8a
Stacey Strovink <staceystrovink@gmail.com>
2020-03-10 18:58:35 -0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+// Business Logic
+
+function pigLatin(str) {
+  str = str.toLowerCase()
+  const vowels = ["a","e","i","o","u"];
+  let vowelIndex = 0;
+
+  if (vowels.includes(str[0])) {
+    return str + "way";
+  } else {
+    for (let char of str) {
+      if (vowels.includes(char)) {
+        vowelIndex = str.indexOf(char);
+        break;
+      }
+    }
+    return str.slice(vowelIndex) + str.slice(0, vowelIndex) + "ay";
+  }
+}
+
+// User Logic
+
+$(document).ready(function() {
+  $("form#pig-latin").submit(function(event) {
+    event.preventDefault();
+    var word = $("input#word").val();
+    var result = pigLatin(word);
+    $("#result").text(result);
+  });
+});
